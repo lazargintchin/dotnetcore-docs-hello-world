2fa8f5376a0c999ff36ff46f94443cdf19ce2e5a
lazargintchin <lazar@skedda.com>
2023-01-04 18:32:25 -0700

    Added a function to place a queue message

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Runtime.InteropServices;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.Storage;
+using Microsoft.Azure.Storage.Queue;
 
 namespace dotnetcoresample.Pages;
 
@@ -17,6 +21,18 @@ public class IndexModel : PageModel
     }
 
     public void OnGet()
-    {        
+    {
+        placeMessageOnQueue();
+    }
+    
+    public void placeMessageOnQueue()
+    {
+        CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=lazarstorage;AccountKey=XsQnGVw4M2b7IosvmPhUZVRrUl8To+TDB0ioirQyv6U7TxsKkSaABN3SMmBZOfbtbfA8VCeUYaM1+AStUoYkww==;EndpointSuffix=core.windows.net");
+        CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+        CloudQueue queue = queueClient.GetQueueReference("lazarq");
+
+        string messageContent = "This is a test message";
+        CloudQueueMessage message = new CloudQueueMessage(messageContent);
+        queue.AddMessage(message);
     }
 }
